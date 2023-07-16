73f75687c590cdc0074c77f1722d99e36b45204b
Tejas Chendekar <tejaschendekar@RBL-MAC-TEJAS-TECH.local>
2023-07-17 01:18:20 +0530

    Added feed fetcher and loader

--- dags/feedscrapper.py
+++ dags/feedscrapper.py
@@ -5,7 +5,7 @@ Initialize a web scrapping dag to collect data and push to a MongoDB data base
 from datetime import datetime
 from airflow.decorators import dag, task
 from airflow.providers.redis.hooks.redis import RedisHook
-from helper import webScrapperHelper
+from helper import feedhelper
 from airflow.operators.python import PythonOperator
 
 
@@ -18,26 +18,23 @@ configuration: dict = {
     start_date=datetime(2023, 7, 16), max_active_runs=3, catchup=False, description= 'Web Scrapper',
     schedule_interval= '*/60 * * * * *'
 )
-def webScrap():
+def collectFeeds():
     URL_SET_KEY = 'url_set'
 
     #Gets list of URLs to scrape from Redis
     @task(task_id='get_urls')
-    def getURLS():
+    def getFeedData():
         redisConn = RedisHook(configuration.get('REDIS_CONN_ID')).get_conn()
         urlSet = redisConn.smembers(URL_SET_KEY)
-        print('[DAG][webScrap][getURLS] redis urlSet', urlSet)
+        print('[DAG][collectFeeds][getFeedData] redis urlSet', urlSet)
         if urlSet == None:
             return None
 
-        filteredUrlList = webScrapperHelper.filterUnreachableWebsites(urlSet=urlSet)
-            
-        print('[DAG][webScrap][getURLS][filtered] redis urlSet',filteredUrlList)
+        filteredUrlList = feedhelper.loadFeeds(urlSet=urlSet) 
 
         return None
         
-    getURLS()
+    getFeedData()
 
 
-
-webScrap()
+collectFeeds()
